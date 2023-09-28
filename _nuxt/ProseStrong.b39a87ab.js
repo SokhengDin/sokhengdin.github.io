795e04d39f51bf3163608e752e4404c3dffbaa04
SokhengDin <din.sokheng08@gmail.com>
2023-09-28 16:55:59 +0700

    Updates

---
+++ _nuxt/ProseStrong.b39a87ab.js
@@ -0,0 +1,1 @@
+import{_ as o,o as r,c as t,$ as n}from"./entry.a6b72f3d.js";const s={};function c(e,a){return r(),t("strong",null,[n(e.$slots,"default")])}const _=o(s,[["render",c]]);export{_ as default};
